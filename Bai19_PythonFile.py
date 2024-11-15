ebfcbe1e6fe81d9d80e126240cf726961d6e4a43
vudkhoa <vudinhkhoaowl@gmail.com>
2024-11-15 14:34:52 +0700

    Update

--- Bai19_PythonFile.py
+++ Bai19_PythonFile.py
@@ -1,5 +1,15 @@
+def isPythonFile(s): 
+    if s[-3:] != ".py" and s[-3:] != ".PY" and s[-3:] != ".Py" and s[-3:] != ".pY":
+        return "no"
+    fileName = s[:-3]
+    for index in range(len(fileName) - 3): 
+        if (fileName[index] < 'a' or fileName[index] > 'z') and (fileName[index] < 'A' or fileName[index] > 'Z') and fileName[index] != '_':
+            return "no"
+    return 'yes' 
+
 def main(): 
-    pass
+    s = input()    
+    print(isPythonFile(s))
 
 if __name__ == "__main__":
     main()
